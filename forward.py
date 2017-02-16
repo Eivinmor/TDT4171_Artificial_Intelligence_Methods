24a8c38999bdfea7dfa28a53dc8785733f155940
Eivind Morch <eivinmor@stud.ntnu.no>
2017-02-16 21:07:02 +0100

    Seperated forward so other file

---
+++ forward.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+T = np.matrix([[0.7, 0.3],
+               [0.3, 0.7]])
+
+O_true = np.matrix([[0.9, 0],
+                    [0, 0.2]])
+
+O_false = np.matrix([[0.1, 0],
+                     [0, 0.8]])
+
+ev = [None, 1, 1, 0, 1, 1]
+
+
+def forward(prev_msg, evidence):
+    prediction = get_O(evidence) * np.transpose(T) * prev_msg   # O_t * T(transposed) * f_{t-1}
+    normalisation = prediction.sum()                            # Calculate normalisation
+    return prediction / normalisation                           # Return normalised values
+
+
+def forward_algorithm(t):
+    t += 1
+    fv = [0]*t
+    fv[0] = np.matrix([[0.5], [0.5]])
+    for i in range(1, t):
+        print("\nIteration ", i, "\n---------------")
+        fv[i] = forward(fv[i-1], ev[i])
+        print(fv[i])
+        print("---------------")
+
+
+# noinspection PyPep8Naming
+def get_O(umbrella):
+    if umbrella:
+        return O_true
+    else:
+        return O_false
+
+
+forward_algorithm(5)
